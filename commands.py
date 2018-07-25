7f5c6ea35bc720046d28db4195d8e9f0cc7e7c49
mica <mica.eldridge1@gmail.com>
2018-07-24 23:41:50 -0700

    initial commit

---
+++ commands.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+"""
+
+Version 0.1
+2018-07-24
+"""
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import datetime
+import subprocess
+import re
+import argparse
+
+from sqlalchemy import DDL, func
+from sqlalchemy.orm import sessionmaker
+
+from network_tracker_config import engine
+from models import Helper, Entry, SABase
+
+
+arp_line_re = re.compile(
+    '(?P<ip_address>'
+        '(\d{1,3}\.){3}\d{1,3}'
+    ')'
+    '\t'
+    '(?P<mac_address>'
+        '([\d\w]{2}:){5}[\d\w]{2}'
+    ')'
+    '\t'
+    '(?P<name>'
+        '[^\n]+'
+    ')',
+    re.I,
+)
+
+def arp_scan(interface='en0'):
+    return subprocess.check_output(
+        'arp-scan --localnet --interface {interface}'.format(
+            interface=interface,
+        ),
+        shell=True,
+    ).decode().strip()
+
+def run_update():
+    sess = sessionmaker(bind=engine)()
+
+    Helper.set_sess(sess)
+    now = datetime.datetime.now()
+
+    most_recent_record_time = sess.query(func.max(Entry.timeto)).one_or_none()
+
+    last_by_mac_connected_and_not_connected = {
+        e.device.mac: e
+        for e in sess.query(Entry).filter(Entry.timeto==most_recent_record_time).all()
+    }
+
+    scan = arp_scan()
+
+    lines = scan.split('\n')
+
+    added_to_network = list()
+    stayed_in_network = list()
+
+    seen_macs = set()
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        m = arp_line_re.match(line)
+        if not m:
+            continue
+        ip_address = m.group('ip_address')
+        mac_address = m.group('mac_address')
+        name = m.group('name')
+
+        if mac_address in seen_macs:
+            continue
+        seen_macs.add(mac_address)
+
+        # stayed in network
+        if mac_address in last_by_mac_connected_and_not_connected:
+            entry = last_by_mac_connected_and_not_connected.pop(mac_address)
+            stayed_in_network.append((entry, ip_address, name))
+        else:
+            added_to_network.append((ip_address, mac_address, name))
+
+    # stayed in network, but check if anything changed.
+    for entry, ip_address, name in stayed_in_network:
+        if entry.ip.ip != ip_address or entry.device.arp_name != name:
+            sess.add(Entry(
+                'connected', ip_address, mac_address, name, now,
+            ))
+        else:
+            entry.timeto = now
+
+    for ip_address, mac_address, name in added_to_network:
+        sess.add(Entry(
+            'connected', ip_address, mac_address, name, now
+        ))
+
+    for entry in last_by_mac_connected_and_not_connected.values():
+        if entry.status.status == 'connected':
+            sess.add(Entry(
+                'not connected', entry.ip.ip, entry.device.mac, entry.device.arp_name, now
+            ))
+        else:
+            entry.timeto = now
+
+    sess.commit()
+
+
+def create_tables():
+    engine.execute(DDL('CREATE SCHEMA IF NOT EXISTS network'))
+    SABase.metadata.create_all()
+    engine.execute(DDL('''
+    DROP VIEW network_history;
+    CREATE OR REPLACE VIEW network_history AS
+        select
+            e.eid
+            , e.timefrom
+            , e.timeto
+            , s.status
+            , ip.ip
+            , d.mac
+            , d.name
+        from entry e
+        inner join status s using(sid)
+        inner join ip using(ipid)
+        inner join devices d using(did)
+        where e.timeto=(select max(timeto) from entry)
+        order by regexp_replace(ip, '.*\.', '')::int
+    ;
+    '''))
+
+def drop_tables():
+    SABase.metadata.drop_all()
+
+
+def run_main():
+    args = parse_cl_args()
+
+    if args.run_update:
+        run_update()
+    elif args.create_tables:
+        create_tables()
+    elif args.drop_tables:
+        drop_tables()
+
+    success = True
+    return success
+
+def parse_cl_args():
+    argParser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    argParser.add_argument(
+        '--run-update',
+        default=False,
+        action='store_true',
+    )
+    argParser.add_argument(
+        '--create-tables',
+        default=False,
+        action='store_true',
+    )
+    argParser.add_argument(
+        '--drop-tables',
+        default=False,
+        action='store_true',
+    )
+
+    args = argParser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    success = run_main()
+    exit_code = 0 if success else 1
+    exit(exit_code)
---
+++ models.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+from sqlalchemy import (
+    BigInteger, Text, Integer, DateTime,
+    ForeignKey,
+    Column
+)
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.schema import MetaData
+from sqlalchemy.orm import sessionmaker, relationship
+
+from network_tracker_config import engine
+
+
+SABase = declarative_base(
+    metadata=MetaData(
+        bind=engine,
+        schema='network',
+    ),
+)
+
+
+class Helper:
+    @classmethod
+    def get_row(cls, column, value):
+        sess = cls.get_sess()
+        query = sess.query(cls).filter(column==value)
+
+        # if it exists, then return it
+        row = query.one_or_none()
+        if row is not None:
+            return row
+
+        # otherwise, create one
+        row = cls()
+        setattr(row, str(column).split('.')[-1], value)
+
+        sess = cls.get_sess()
+        sess.add(row)
+        sess.commit()
+
+        return row
+
+    sess = None
+    @classmethod
+    def set_sess(cls, sess):
+        cls.sess = sess
+
+    @classmethod
+    def get_sess(cls):
+        if cls.sess.is_active:
+            return cls.sess
+        cls.sess = sessionmaker(bind=cls.metadata.bind)()
+        return cls.sess
+
+
+class IPAddress(SABase, Helper):
+    @classmethod
+    def get_row(cls, ip_address):
+        return super(IPAddress, cls).get_row(cls.ip, ip_address)
+
+    __tablename__ = 'ip'
+    ipid = Column(
+        'ipid',
+        Integer,
+        autoincrement=True,
+        doc='ip address id',
+        primary_key=True,
+    )
+    ip = Column(
+        'ip',
+        Text,
+        unique=True,
+    )
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            self.ip,
+        )
+    def __str__(self):
+        return repr(self)
+
+
+class Status(SABase, Helper):
+    @classmethod
+    def get_row(cls, status_str):
+        return super(Status, cls).get_row(cls.status, status_str)
+
+    __tablename__ = 'status'
+    sid = Column(
+        'sid',
+        Integer,
+        autoincrement=True,
+        doc='status name id',
+        primary_key=True,
+    )
+    status = Column(
+        'status',
+        Text,
+        unique=True,
+    )
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            self.status,
+        )
+    def __str__(self):
+        return repr(self)
+
+
+class Device(SABase, Helper):
+    @classmethod
+    def get_row(cls, mac_address):
+        return super(Device, cls).get_row(cls.mac, mac_address)
+
+    __tablename__ = 'devices'
+    did = Column(
+        'did',
+        Integer,
+        autoincrement=True,
+        primary_key=True,
+    )
+    mac = Column(
+        'mac',
+        Text,
+        unique=True,
+    )
+    name = Column(
+        'name',
+        Text,
+    )
+    arp_name = Column(
+        'arp_name',
+        Text,
+    )
+    def __repr__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            self.mac,
+        )
+    def __str__(self):
+        return repr(self)
+
+
+class Entry(SABase, Helper):
+    __tablename__ = 'entry'
+    eid = Column(
+        'eid',
+        BigInteger,
+        autoincrement=True,
+        primary_key=True,
+    )
+    timefrom = Column(
+        'timefrom',
+        DateTime,
+        nullable=False,
+        default='now()',
+    )
+    timeto = Column(
+        'timeto',
+        DateTime,
+        nullable=False,
+        default='now()',
+    )
+
+    sid = Column('sid', Integer, ForeignKey('status.sid'))
+    status = relationship('Status')
+
+    ipid = Column('ipid', Integer, ForeignKey('ip.ipid'))
+    ip = relationship('IPAddress')
+
+    did = Column('did', Integer, ForeignKey('devices.did'))
+    device = relationship('Device')
+
+    def __init__(self, status_str, ip_address, mac_address, arp_name, time):
+        # if status doesn't exist, create it
+        status = Status.get_row(status_str)
+        self.sid = status.sid
+
+        ip = IPAddress.get_row(ip_address)
+        self.ipid = ip.ipid
+
+        device = Device.get_row(mac_address)
+        device.arp_name = arp_name
+        self.get_sess().commit()
+        self.did = device.did
+
+        self.timefrom = self.timeto = time
+
+    def __str__(self):
+        return '{}({})'.format(
+            self.__class__.__name__,
+            ', '.join((repr(attr) for attr in [
+                self.status.status,
+                self.ip.ip,
+                self.device.mac,
+                self.timefrom,
+            ]))
+        )
+    def __repr__(self):
+        return str(self)
